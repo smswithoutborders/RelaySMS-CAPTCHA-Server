25819edb5287c0ff47a0bea0e4956c80f14ce13d
Promise Fru <info@promisefru.com>
2025-10-31 15:34:02 +0100

    Fix application module reference in start script for Uvicorn

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 . venv/bin/activate
 
-uvicorn main:app --host 127.0.0.1 --port 8080 --workers 1 --proxy-headers --forwarded-allow-ips='*'
+uvicorn app:app --host 127.0.0.1 --port 8080 --workers 1 --proxy-headers --forwarded-allow-ips='*'
